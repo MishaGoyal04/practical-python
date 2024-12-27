b26a975691a894c4dcbf585d8d9b1509dbaf963e
MishaGoyal04 <122969822+MishaGoyal04@users.noreply.github.com>
2024-12-27 16:54:58 +0530

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
-#
+initial_height = 100
+bounce_factor = 3 / 5
+
+print("Bounce    Height (m)")
+
+current_height = initial_height 
+
+for bounce in range (1, 11):
+  current_height *= bounce_factor
+  print (f"{bounce:<8} {current_height:.2f}")
 # Exercise 1.5
